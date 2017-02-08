ebb706aadd866182b47cf1cf87d729f52dc26aac
= <=>
2017-02-08 15:20:18 +0100

    shopping cart home

--- server.js
+++ server.js
@@ -2,4 +2,16 @@
  * Created by naimi on 2/8/17.
  */
 
-var 
+var express = require("express");
+var path = require("path");
+
+var app  = express();
+
+app.set("views", path.join(__dirname,"view"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+   res.render("index");
+});
+
+app.listen(3000);
