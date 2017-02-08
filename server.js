dc64e568e48ca8834d0dd97b23dbf75e26c7cbd0
= <=>
2017-02-08 16:29:33 +0100

    API ready to be consumed

--- server.js
+++ server.js
@@ -4,11 +4,15 @@
 
 var express = require("express");
 var path = require("path");
+var APIv1 = require("./routes/APIv1");
 
 var app  = express();
 
 app.set("views", path.join(__dirname,"view"));
 app.set("view engine", "ejs");
+
+//use the APIv1 routes
+app.use("/api/v1", APIv1);
 
 app.get("/", function (req, res) {
    res.render("index");
